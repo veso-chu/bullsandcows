c15a498820b02c1dad575d8effade05aa4c012a8
Veselin Chumpalov <v.chumpalov@proxiad.com>
2022-12-07 12:50:33 +0200

    changed Game constructor to set goal through setter

--- src/main/java/com/proxiad/bullsandcows/game/Game.java
+++ src/main/java/com/proxiad/bullsandcows/game/Game.java
@@ -7,36 +7,36 @@ import jakarta.validation.constraints.NotEmpty;
 public class Game {
 
 	private UUID id;
-	
+
 	@NotEmpty
 	@GameGoalConstraint
 	private String goal;
-	
+
 	public Game(String goal) {
 		this.setId();
-		this.goal = goal;
+		this.setGoal(goal);
 	}
-	
+
 	public UUID getId() {
 		return id;
 	}
-	
+
 	public void setId() {
 		this.id = UUID.randomUUID();
 	}
-	
+
 	public String getGoal() {
 		return goal;
 	}
-	
+
 	public void setGoal(String goal) {
 		this.goal = goal;
 	}
-	
+
 //	public List<String> getGuesses() {
 //		return guesses;
 //	}
-//	
+//
 //	public void setGuesses(List<String> guesses) {
 //		this.guesses = guesses;
 //	}
